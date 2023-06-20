716207a2fbb95f28fedc015c2da03a856d7af9f6
Shali <3401941092@qq.com>
2023-06-20 22:21:54 +0800

    Commit

--- Project9.1/src/main/java/com/atguigu/mapper/UserMapper.java
+++ Project9.1/src/main/java/com/atguigu/mapper/UserMapper.java
@@ -1,5 +1,17 @@
 package com.atguigu.mapper;
 
+import com.atguigu.pojo.User;
+
+import java.util.List;
+
 public interface UserMapper {
     public abstract int insertUser();
+
+    public abstract void deleteUser();
+
+    int updateUser();
+
+    User getUserById();
+
+    List<User> getAllUsers();
 }
--- Project9.1/src/test/java/com/atguigu/test1/InsertTest.java
+++ Project9.1/src/test/java/com/atguigu/test1/InsertTest.java
@@ -1,6 +1,7 @@
 package com.atguigu.test1;
 
 import com.atguigu.mapper.UserMapper;
+import com.atguigu.pojo.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -8,6 +9,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class InsertTest {
     @Test
@@ -19,5 +21,18 @@ public class InsertTest {
         sqlSession.commit();
         System.out.println(res);
         sqlSession.close();
+    }
+
+    @Test
+    public void test2() throws IOException {
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSession sqlSession = new SqlSessionFactoryBuilder().build(is).openSession(true);
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+//        mapper.updateUser();
+//        mapper.deleteUser();
+//        User user = mapper.getUserById();
+//        System.out.println(user);
+        List<User> users = mapper.getAllUsers();
+        System.out.println(users);
     }
 }
